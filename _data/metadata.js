89012d5ced2c644ea83140b65cd3b4a4363fec1f
kinshukshah <kinshukshah@gmail.com>
2023-10-11 23:31:52 +0530

    data cleanup and social links

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,12 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "Kinshuk Shah",
+	url: "https://kinshukshah.dev/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "I write about everything around JavaScript.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Kinshuk Shah",
+		email: "kinshukshah@gmail.com",
+		twitter:"@shahkinshuk",
+		url: "https://kinshukshah.dev/about/"
 	}
 }
